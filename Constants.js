738511826cf43d3894064a7492d9f4bf013c841f
Skr GS <scarynomi@gmail.com>
2022-03-16 04:29:03 +0800

    a

--- Constants.js
+++ Constants.js
@@ -2,7 +2,7 @@ import { Dimensions } from "react-native";
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
-export const api = "http://192.168.1.67:4040";
+export const api = "http://157.245.198.129";
 
 export const fullWidth = { windowWidth };
 export const fullHeight = { windowHeight };
